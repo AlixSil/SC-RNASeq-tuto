e12a1ea2d8e7b0141985082b872809b2635c297c
Alix Silvert <alix.silvert@protonmail.com>
2021-08-19 11:16:47 +0200

    creating first files

---
+++ scripts/01_load_data_into_seurat.R
@@ -0,0 +1,16 @@
+###
+#The way you load data into Seurat can vary depending on how you aligned and counted your reads
+#Here, we're going to go thourgh several examples of various tools such as 10XRanger, Alevin etc ...
+###
+
+##In the follow up, we will only use the 10X Ranger version
+
+library(Seurat)
+
+##1OX Cell Ranger
+
+
+##Alevin
+
+
+##TODO : add others
---
+++ scripts/02_pre-processing.R
@@ -0,0 +1,8 @@
+##Before we can analyse the data, some pre processing is necessary
+##the read of bad quality have been removed, but for examples, some cells have too low "features" (i.e. genes)
+
+library(Seurat)
+
+NSCLC_DTC.data = Read10X(data.dir = "../input_data/10X_CellRanger/")
+
+print("poutoup")
