eada2872e9c23085afd0dc44e843549c1297629b
Alix Silvert <alix.silvert@protonmail.com>
2021-08-20 09:30:38 +0200

    pre-processing done

--- scripts/02_pre-processing.R
+++
@@ -1,8 +0,0 @@
-##Before we can analyse the data, some pre processing is necessary
-##the read of bad quality have been removed, but for examples, some cells have too low "features" (i.e. genes)
-
-library(Seurat)
-
-NSCLC_DTC.data = Read10X(data.dir = "../input_data/10X_CellRanger/sample_feature_bc_matrix/")
-str(NSCLC_DTC.data)
-print("poutoup")
